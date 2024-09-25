3c8f59a5b2f1f54deaeddc4ac97aeef8ef628c2c
Diogo Oliveira <dluks82@gmail.com>
2024-09-25 00:17:32 -0300

    feat: Add `MainMenuScreen`

---
+++ src/main/java/ui/screens/MainMenuScreen.java
@@ -0,0 +1,87 @@
+package ui.screens;
+
+import repository.*;
+import service.*;
+import ui.core.Screen;
+import ui.flow.FlowController;
+import ui.screens.agency.AgencyMenuScreen;
+import ui.screens.customer.CustomerMenuScreen;
+import ui.screens.rental.RentalMenuScreen;
+import ui.screens.vehicle.VehicleMenuScreen;
+import ui.utils.Input;
+import ui.utils.Output;
+import ui.utils.ScreenUtils;
+
+import java.util.Scanner;
+
+public class MainMenuScreen extends Screen {
+    private final Scanner scanner;
+
+    private final AgencyService agencyService;
+//    private final VehicleService vehicleService;
+//    private final CustomerService customerService;
+//    private final RentalService rentalService;
+
+    public MainMenuScreen(FlowController flowController,
+                          Scanner scanner) {
+        super(flowController);
+        this.scanner = scanner;
+
+        AgencyRepository agencyRepository = new InMemoryAgencyRepositoryImpl();
+//        VehicleRepository vehicleRepository = new InMemoryVehicleRepository();
+//        CustomerRepository customerRepository = new InMemoryCustomerRepositoryImpl();
+//        RentalRepository rentalRepository = new InMemoryRentalRepository();
+
+        agencyService = new AgencyServiceImpl(agencyRepository);
+//        vehicleService = new VehicleServiceImpl(vehicleRepository);
+//        customerService = new CustomerServiceImpl(customerRepository);
+//        rentalService = new RentalServiceImpl(rentalRepository);
+
+    }
+
+    @Override
+    public void show() {
+        int option;
+
+        do {
+            ScreenUtils.clearScreen();
+
+            System.out.println("Coders Rental App");
+            System.out.println("1 - Agencias");
+            System.out.println("2 - Veículos");
+            System.out.println("3 - Clientes");
+            System.out.println("4 - Locações");
+            System.out.println("0 - Exit");
+
+            option = Input.getAsInt(scanner, "Escolha uma opção: ", false);
+
+            switch (option) {
+                case 1:
+                    flowController.goTo(
+                            new AgencyMenuScreen(flowController, scanner, agencyService));
+                    break;
+                case 2:
+//                    flowController.goTo(
+//                            new VehicleMenuScreen(flowController, scanner, vehicleService));
+                    break;
+                case 3:
+//                    flowController.goTo(
+//                            new CustomerMenuScreen(flowController, scanner, customerService));
+                    break;
+                case 4:
+//                    flowController.goTo(
+//                            new RentalMenuScreen(flowController, scanner, rentalService));
+                    break;
+                case 0:
+                    Output.info("Encerrando App...");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+                    Output.error("Opção inválida!");
+                    scanner.nextLine();
+                    break;
+            }
+        } while (true);
+    }
+}
